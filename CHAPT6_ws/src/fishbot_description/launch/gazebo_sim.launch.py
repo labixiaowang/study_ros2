c37ed0e67b7be331850f00f7012567c1c5556552
labixiaowang <2813515886@qq.com>
2025-03-20 22:35:15 +0800

    2025-03-20

--- CHAPT6_ws/src/fishbot_description/launch/gazebo_sim.launch.py
+++ CHAPT6_ws/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -8,12 +8,13 @@ import launch_ros.parameter_descriptions
 def generate_launch_description():
     # 获取默认的URDF文件路径
     urdf_package_path = get_package_share_directory('fishbot_description')
-    default_urdf_file_path =os.path.join(urdf_package_path, 'urdf', 'fishbot.urdf') #拼接路径
+    default_xacro_file_path =os.path.join(urdf_package_path, 'urdf', 'robot/robot.urdf.xacro') #拼接路径
     # default_urdf_rviz_config_file_path = os.path.join(urdf_package_path, 'config','display_robot_model.rviz') #拼接路径
+    default_gazebo_world_file_path = os.path.join(urdf_package_path, 'world','custom_room.world')
     # 申明URDF文件的参数
     action_declare_arg_model_path = launch.actions.DeclareLaunchArgument(
         'model',
-        default_value=str(default_urdf_file_path),
+        default_value=str(default_xacro_file_path),
         description='Absolute path to robot urdf file')
     # 通过文件路径转换为参数，传入robot_state_publisher节点
     # substitutions_commend_result=launch.substitutions.Command(['cat ', launch.substitutions.LaunchConfiguration('model')])
@@ -26,21 +27,32 @@ def generate_launch_description():
             parameters=[{'robot_description': robot_description_value}],
         )
     
-    joint_state_publisher = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable ='joint_state_publisher',
+    # joint_state_publisher = launch_ros.actions.Node(
+    #     package='joint_state_publisher',
+    #     executable ='joint_state_publisher',
+    # )
+
+    # action_rviz_node = launch_ros.actions.Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     # arguments=['-d', default_urdf_rviz_config_file_path],
+    # )
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'), '/launch/gazebo.launch.py']
+            
+        ),
+        launch_arguments= [ ('world', default_gazebo_world_file_path),('verbose','true')]
     )
 
-    action_rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        # arguments=['-d', default_urdf_rviz_config_file_path],
+    action_spwn_entity = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','/robot_description','-entity','fishbot']
     )
-
     return launch.LaunchDescription([
         action_declare_arg_model_path,
         action_robot_state_publisher,
-        joint_state_publisher,
-        action_rviz_node
-        
+        action_launch_gazebo,
+        action_spwn_entity
     ])
